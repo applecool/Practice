a05e3a12ac23c0a9111a9181b5b071a2fb86cf17
applecool <kmitharsha@gmail.com>
2014-11-22 22:23:14 -0600

    Added charexp.cpp

---
+++ charexp.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+char c = 'H';
+
+cout << c << endl;
+
+cout << 'H';
+cout << "\n";
+cout << "M";
+cout << "\n";
+
+cout.put(c);
+cout << "\n";
+
+return 0;
+
+}
