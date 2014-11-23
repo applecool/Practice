ed4c2191c3744ab24a7e8be21f07156cf961e597
applecool <kmitharsha@gmail.com>
2014-11-22 22:22:46 -0600

    Added arrtest.cpp

---
+++ arrtest.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+int arr[20]={1000};
+
+cout << arr[0]<<"\n";
+cout << arr[1]<< "\n";
+cout << arr[2]<< "\n";
+cout << arr[19]<< "\n";
+
+return 0;
+
+}
