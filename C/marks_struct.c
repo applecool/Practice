eae743a09b9e8bd19a1ed59147fb8336d1faff92
applecool <kmitharsha@gmail.com>
2014-11-22 22:55:57 -0600

    added comments to marks_struct.c

--- C/marks_struct.c
+++ C/marks_struct.c
@@ -1,10 +1,17 @@
+/* Create a student structure which contain 3 fields such as Rollno, Marks and
+   name of a student.
+   Enter the details of a student by user as Rollno,name and 3 subject marks
+   and name of the student
+  a)Find the total marks of a student?
+  b)Find highest marks among the 3 subjects? */
+
 #include <stdio.h>
 struct result
 {
 	int rno, mrks[5];
 	char nm;
 }res;
-void main()
+int main()
 {
 	int i,total;
 	total = 0;
@@ -24,4 +31,5 @@ void main()
 	printf("marks:%d\n",res.mrks[i]);
 	}
 	printf("\n\n\t Total is : %d",total);
+	return 0;
 }
