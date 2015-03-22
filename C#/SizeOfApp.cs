91244fd4e201b2d206de578b89320f44c4c37755
applecool <kmitharsha@gmail.com>
2015-03-21 21:31:30 -0500

    Added SizeOfApp.cs

---
+++ C#/SizeOfApp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Application
+{
+	public class SizeOfApp
+	{
+		static void Main(string[] args){
+
+			Console.WriteLine ("The size of the int is :" + sizeof(int));
+			Console.WriteLine ("The size of the float is :" + sizeof(float));
+			Console.WriteLine ("The size of the double is :" + sizeof(double));
+			Console.WriteLine ("The size of the char is :" + sizeof(char));
+			Console.ReadLine ();
+		}
+	}
+}
+
