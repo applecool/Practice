0a492195655a3501c1de79070086d3ce0c44a11b
applecool <kmitharsha@gmail.com>
2015-03-21 21:44:04 -0500

    Added TypeConversionApp

---
+++ C#/ExplicitConversion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TypeConversionApplication
+{
+	public class ExplicitConversion
+	{
+		static void Main(string[] args){
+
+			double num = 7983.562;
+
+			Console.WriteLine ("Before typecasting : "+ num);
+
+			int n;
+
+			n = (int)num;// cast double to integer
+
+			Console.WriteLine ("After typecasting: "+ n);
+		}
+	}
+		
+}
+
---
+++ C#/StringConversion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TypeConversionApplication
+{
+	public class StringConversion
+	{
+		static void Main(string[] args){
+		
+			int i = 10;
+			double j = 111234.897;
+			float f = 12.8f;
+			bool b = false;
+
+			Console.WriteLine ("Converting int to string :" + i.ToString());
+			Console.WriteLine ("Converting double to string :" + j.ToString());
+			Console.WriteLine ("Converting float to string :" + f.ToString());
+			Console.WriteLine ("Converting boolean to string :" + b.ToString());
+		}
+	}
+}
+
