80b610a416644108b3c59c3638b72770df6076c9
applecool <kmitharsha@gmail.com>
2015-08-12 13:33:04 -0400

    added keypad permutations

---
+++ C#/KeypadPermutations.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Application
+{
+	public class KeypadPermutations
+	{
+		public int Factorial(int num){
+
+			if (num == 1 || num == 0) {
+				return 1;
+			} else {
+				
+				return num * Factorial (num - 1);
+			}
+
+		}
+
+		public int ComputePermutation(int i){
+
+			int n;
+			int perm;
+
+			perm = Factorial (n) / Factorial (n - i);
+
+
+		}
+
+		static void Main(string[] args){
+			KeypadPermutations k = new KeypadPermutations();
+			Console.WriteLine("Fact is :" + k.Factorial (1));
+			Console.ReadLine ();
+		}
+	}
+}
+
