d66c6fb7cf1995d4b7f380884984d7ad3ed1d779
applecool <kmitharsha@gmail.com>
2016-01-13 14:08:11 +0530

    fixed formatting

--- JS/mindbending_array.js
+++ JS/mindbending_array.js
@@ -1,11 +1,18 @@
 /*“What is obtained when the result of passing 9 into the fourth function of the puzzlers array is then passed into the function whose array index matches the result of passing 3 into the second function of the puzzlers array? */
 
 var puzzlers = [
-  function(a) { return 8 * a - 10; },
-  function(a) { return (a - 3) * (a - 3) * (a - 3); },
-  function(a) { return a * a + 4; },
-  function(a) { return a % 5; }
+  function(a) {
+    return 8 * a - 10;
+  },
+  function(a) {
+    return (a - 3) * (a - 3) * (a - 3);
+  },
+  function(a) {
+    return a * a + 4;
+  },
+  function(a) {
+    return a % 5;
+  }
 ];
 
 alert(puzzlers[puzzlers[1](3)](puzzlers[3](9)));
-
