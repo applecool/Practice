9429c79e60458b7a279fb1ce6eed3fada14676a7
applecool <kmitharsha@gmail.com>
2016-01-21 11:37:30 -0600

    added more properties

--- JS/OOPS/oc.js
+++ JS/OOPS/oc.js
@@ -1,11 +1,11 @@
+// Take on Pirates
+
 var vehicle1 = {type: "Motorboat", capacity: 6, storedAt: "Ammunition Depot"};
 var vehicle2 = {type: "Jet Ski", capacity: 1, storedAt: "Reef Dock"};
 var vehicle3 = {type: "Submarine", capacity: 8, storedAt: "Underwater Outpost"};
 
-// create vehicles array
 var vehicles = [vehicle1, vehicle2, vehicle3];
 
-// build findVehicle function expression
 var findVehicle= function(name, list){
   for(var i = 0; i <list.length; i++){
     if(list[i].type == name ){
@@ -14,5 +14,17 @@ var findVehicle= function(name, list){
   }
 };
 
-// call findVehicle
 findVehicle("Submarine", vehicles);
+
+vehicle1.capacity = 10;
+vehicle2.submersible = false;
+vehicle3.weapon = "Torpedoes";
+vehicle1.submersible = false;
+vehicle2.weapon = "Lasers";
+vehicle3.capacity = 16;
+vehicle1.weapon = "Rear-Mounted Slingshot";
+vehicle3.submersible = true;
+
+vehicle1["# of weapons"] = 1;
+vehicle2["# of weapons"] = 4;
+vehicle3["# of weapons"] = 8;
