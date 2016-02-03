0852717affefe14653b41ed3c37b08115141d3d4
applecool <kmitharsha@gmail.com>
2016-02-02 20:23:36 -0600

    updated closure

--- JS/Function_Expressions/expression_fn.js
+++ JS/Function_Expressions/expression_fn.js
@@ -2,18 +2,19 @@ var greeting;
 
 var newCustomer = true;
 
-if(newCustomer){
-    greeting = function(){
-        alert("Welcome Amigos");
-    };
-}else{
-    greeting = function(){
-      alert("Welcome back Amigos");  
-    };
+if (newCustomer) {
+  greeting = function() {
+    alert("Welcome Amigos");
+  };
+} else {
+  greeting = function() {
+    alert("Welcome back Amigos");
+  };
 }
 
 
 closingMessage(greeting);
-function closingMessage(message){
-    message();
+
+function closingMessage(message) {
+  message();
 }
